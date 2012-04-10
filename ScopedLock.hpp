68281fef4c02bc2c540f3f3038af21032838c72c
Damien Dagnet <dagnet.damien@gmail.com>
2012-04-10 14:42:59 +0200

    ScopedLock class implemented

---
+++ ScopedLock.hpp
@@ -0,0 +1,23 @@
+#ifndef SCOPED_LOCK_HPP
+# define	SCOPED_LOCK_HPP
+
+#include "Mutex.hpp"
+
+class ScopedLock
+{
+public:
+  ScopedLock(Mutex *mutex) : _mutex(mutex) {
+    this->_mutex->lock();
+  }
+  ~ScopedLock() {
+    this->_mutex->unlock();
+  }
+
+private:
+  Mutex	*_mutex;
+
+  ScopedLock(ScopedLock const&) {}
+  ScopedLock &	operator=(ScopedLock const&) { return (*this);}
+};
+
+#endif
